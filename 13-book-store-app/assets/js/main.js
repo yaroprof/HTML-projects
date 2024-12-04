3eac2a735d258c0c940cc8baa820ed8e59f9c5b0
yaroprof <crosskyiv@gmail.com>
2024-12-04 19:29:20 +0200

    nnm

---
+++ 13-book-store-app/assets/js/main.js
@@ -0,0 +1,49 @@
+/*=============== SEARCH ===============*/
+const searchBtn = document.getElementById('search-button'),
+    searchClose = document.getElementById('search-close'),
+    searchContent = document.getElementById('search-content');
+
+if (searchBtn){
+    searchBtn.addEventListener('click', () => {
+        searchContent.classList.add('show-search');
+    })
+}
+
+if(searchClose){
+    searchClose.addEventListener('click', () => {
+        searchContent.classList.remove('show-search')
+    });
+}
+
+
+/*=============== SHOW MENU ===============*/
+
+
+/*=============== LOGIN ===============*/
+
+
+/*=============== ADD SHADOW HEADER ===============*/
+
+
+/*=============== HOME SWIPER ===============*/
+
+
+/*=============== FEATURED SWIPER ===============*/
+
+
+/*=============== NEW SWIPER ===============*/
+
+
+/*=============== TESTIMONIAL SWIPER ===============*/
+
+
+/*=============== SHOW SCROLL UP ===============*/
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== DARK LIGHT THEME ===============*/
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
